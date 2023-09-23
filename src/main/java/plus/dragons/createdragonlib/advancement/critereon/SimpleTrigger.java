991f917a4e76340133e5714e586a61ab2af5d41a
MarbleGate <marblegatekeeper@outlook.com>
2023-09-23 18:03:59 +0800

    0.5.1.e

--- src/main/java/plus/dragons/createdragonlib/advancement/critereon/SimpleTrigger.java
+++ src/main/java/plus/dragons/createdragonlib/advancement/critereon/SimpleTrigger.java
@@ -1,10 +1,8 @@
 package plus.dragons.createdragonlib.advancement.critereon;
 
 import com.google.gson.JsonObject;
-import com.simibubi.create.foundation.advancement.ITriggerable;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.DeserializationContext;
-import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -12,7 +10,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class SimpleTrigger extends AbstractTrigger<SimpleTrigger.Instance> implements ITriggerable {
+public class SimpleTrigger extends AbstractTrigger<SimpleTrigger.Instance> {
 
     public SimpleTrigger(ResourceLocation id) {
         super(id);
--- src/main/java/plus/dragons/createdragonlib/mixin/CreateItemGroupBaseMixin.java
+++ src/main/java/plus/dragons/createdragonlib/mixin/CreateItemGroupBaseMixin.java
@@ -15,12 +15,12 @@ import plus.dragons.createdragonlib.init.FillCreateItemGroupEvent;
 import java.util.List;
 import java.util.function.Function;
 
-@Mixin(AllCreativeModeTabs.RegistrateDisplayItemsGenerator.class)
+@Mixin(targets="com.simibubi.create.AllCreativeModeTabs$RegistrateDisplayItemsGenerator")
 public abstract class CreateItemGroupBaseMixin implements CreativeModeTab.DisplayItemsGenerator {
 
     @Shadow(remap = false)
     @Final
-    private boolean mainTab;
+    private boolean addItems;
 
     @Shadow(remap = false)
     private static void outputAll(CreativeModeTab.Output output, List<Item> items, Function<Item, ItemStack> stackFunc, Function<Item, CreativeModeTab.TabVisibility> visibilityFunc) {
@@ -32,7 +32,7 @@ public abstract class CreateItemGroupBaseMixin implements CreativeModeTab.Displa
                     target = "Lcom/simibubi/create/AllCreativeModeTabs$RegistrateDisplayItemsGenerator;outputAll(Lnet/minecraft/world/item/CreativeModeTab$Output;Ljava/util/List;Ljava/util/function/Function;Ljava/util/function/Function;)V",
                     remap = false))
     private void injectOutput(CreativeModeTab.Output item, List<Item> output, Function<Item, ItemStack> items, Function<Item, CreativeModeTab.TabVisibility> stackFunc) {
-        var event = new FillCreateItemGroupEvent(mainTab, output);
+        var event = new FillCreateItemGroupEvent(addItems, output);
         MinecraftForge.EVENT_BUS.post(event);
         event.apply();
         outputAll(item,output,items,stackFunc);
